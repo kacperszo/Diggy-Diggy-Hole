62b8e30b841bcb6143ac1b73b2498e47229fd168
Kacper Szot <kacperszot1@gmail.com>
2025-10-04 22:36:04 +0200

    Laders

--- scenes/Cave/camera_2d.gd
+++ scenes/Cave/camera_2d.gd
@@ -3,15 +3,24 @@ extends Camera2D
 @export var player: Player
 
 func _ready() -> void:
-	update_position()
-	
+	limit_left = -100000
+	limit_top = -100000
+	limit_right = 100000
+	limit_bottom = 100000
+
 func _process(delta: float) -> void:
 	update_position()
 
-func _physics_process(delta: float) -> void:
-	update_position()
-
 func update_position() -> void:
-	var size: Vector2i = get_viewport_rect().size / zoom
-	var current_cell: Vector2i = Vector2i(player.global_position) / Vector2i(size)	
-	global_position = current_cell * size
+	if not player:
+		return
+	
+	var size: Vector2 = Vector2(get_viewport_rect().size) / zoom
+	
+	# Poprawne obliczenie current_cell z obsługą wartości ujemnych
+	var current_cell: Vector2i = Vector2i(
+		floorf(player.global_position.x / size.x),
+		floorf(player.global_position.y / size.y)
+	)
+	
+	global_position = Vector2(current_cell) * size
--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -1,13 +1,36 @@
 extends CharacterBody2D
 class_name Player
 
-@export var move_speed: float = 100
+@export var move_speed: float = 100.0
+@export var climb_speed: float = 150.0
+@export var gravity: float = 600.0
+@export var objects_layer: TileMapLayer
 
-func _ready() -> void:
-	pass
+var on_ladder := false
 
-func _process(delta: float) -> void:
-	var move_vector: Vector2 = Input.get_vector("move_left","move_right", "move_up", "move_down")
-	velocity = Vector2(move_vector.x,0.0).normalized() * move_speed
+func _physics_process(delta: float) -> void:
+	var move_vector := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	var local_pos := objects_layer.to_local(global_position)
+	var tile_pos := objects_layer.local_to_map(local_pos)
+	var tile_data := objects_layer.get_cell_tile_data(tile_pos)
+	
+	on_ladder = false
+	if tile_data:
+		var is_ladder = tile_data.get_custom_data("ladder")
+		if is_ladder:
+			on_ladder = true
+	
+	if on_ladder:
+		velocity.x = move_vector.x * move_speed
+		velocity.y = move_vector.y * climb_speed
+		if move_vector.length() == 0:
+			velocity = Vector2.ZERO
+	else:
+		velocity.x = move_vector.x * move_speed
+		if not is_on_floor():
+			velocity.y += gravity * delta
+		else:
+			velocity.y = 0
 	
 	move_and_slide()
