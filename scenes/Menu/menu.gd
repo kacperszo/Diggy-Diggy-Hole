edab0d60c75f5e04e199c8c4d7d502f323edba7a
sumo_slonik <jakub.nowakowski@swmansion.com>
2025-10-05 07:11:28 +0200

    add background to menu

--- scenes/Menu/menu.gd
+++ scenes/Menu/menu.gd
@@ -9,7 +9,7 @@ func _on_info_pressed() -> void:
 
 func _on_start_pressed() -> void:
 	
-	var new_scene = load("res://scenes/Cave/Cave.tscn") as PackedScene
+	var new_scene = load("res://scenes/mainGame.tscn") as PackedScene
 	if new_scene:
 		print("Scene loaded: Cave")
 		get_tree().change_scene_to_packed(new_scene)
